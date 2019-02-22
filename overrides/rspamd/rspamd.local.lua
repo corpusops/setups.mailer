8aa022c3d970f66af901628da001a19a4bd053e0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-02-22 21:40:00 +0100

    Out antispam

--- overrides/rspamd/rspamd.local.lua
+++ overrides/rspamd/rspamd.local.lua
@@ -1,3 +1,5 @@
+local rspamd_regexp = require "rspamd_regexp";
+local log = require "rspamd_logger";
 local reconf = config['regexp'];
 local group = "custom";
 {% if cops_mailer_rspamd_lua_whitelisted %}
@@ -20,5 +22,36 @@ reconf['testspam'] = {
   description = "spamtest",
   score = 15.0,
 }
+-- if dest is not on delivered domains and X-SPAM is y
+-- we add a flag that will be catched by postfix header_filters routing to
+-- redirect outgoing spam to a specific address for further manual processing by sysadmins
+local cusourdomains = {
+  rspamd_regexp.create_cached('/[a-zA-Z0-9_.+-]+@{{cops_mailer_ourdomains.replace('\\', '')}}/im'),
+};
+local is_on = rspamd_regexp.create_cached('/^(?:1|y(?:es)?)$/im');
+rspamd_config:register_symbol({
+  name = 'ADD_X_OUTGOING_SPAM',
+  type = 'idempotent',
+  callback = function(task)
+    local parts = task:get_text_parts();
+    local is_domain = false;
+    local pr = task:get_principal_recipient();
+    local spam_action = task:get_metric_action('default');
+    local is_spam = (spam_action ~= 'no action' and action ~= 'greylist')
+    for i, pat in ipairs(cusourdomains) do
+      if pat:match(pr) then
+        is_domain = true;
+        break
+      end
+    end
+    -- task:set_milter_reply({add_headers = {
+    --   ['X-Cops-OutSpam-isdomain'] = tostring(is_domain),
+    --   ['X-Cops-OutSpam-Spam-pr'] = pr,
+    -- }});
+    if is_spam and not is_domain then
+      task:set_milter_reply({add_headers = {['X-Outgoing-Spam'] = '1'}});
+    end
+  end
+});
 --
 {{cops_mailer_rspamd_lua_custom}}
