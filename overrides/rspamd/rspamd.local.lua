b727920552a0084893522836fc5b2a1d314b3b20
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-06-18 20:10:20 +0200

    refresh

--- overrides/rspamd/rspamd.local.lua
+++ overrides/rspamd/rspamd.local.lua
@@ -2,7 +2,37 @@ local rspamd_regexp = require "rspamd_regexp";
 local log = require "rspamd_logger";
 local reconf = config['regexp'];
 local group = "custom";
+local bmevent_re = "/prodid....bluemind..bluemind calendar/im";
 local is_bm_event = rspamd_regexp.create_cached(bmevent_re);
+--
+{% if cops_mailer_rspamd_lua_blacklisted %}
+local blacklistedsenders = {
+{% for i in cops_mailer_rspamd_lua_blacklisted%}
+    "{{i}}",
+{% endfor %}
+};
+reconf['BLACKLISTED_SENDERS'] = {
+  re = string.format("(%s)", table.concat(blacklistedsenders, ") | (")),
+  group = group,
+  description = "not authorized senders",
+  score = 30000.0,
+}
+{% endif %}
+
+{% if cops_mailer_rspamd_lua_whitelisted %}
+local whitelistedsenders = {
+{% for i in cops_mailer_rspamd_lua_whitelisted%}
+    "{{i}}",
+{% endfor %}
+};
+reconf['WHITELIST_CUSTOMERS'] = {
+  re = string.format("(%s)", table.concat(whitelistedsenders, ") | (")),
+  group = group,
+  description = "authorized senders",
+  score = -30000.0,
+}
+{% endif %}
+--
 --
 rspamd_config.WHITELIST_BMEVENT = {
   callback = function(task)
@@ -17,34 +47,6 @@ rspamd_config.WHITELIST_BMEVENT = {
   score = -40.0,
 }
 --
-{% if cops_mailer_rspamd_lua_blacklisted %}
-local blacklistedsenders = {
-    {% for i in cops_mailer_rspamd_lua_blacklisted%}
-    "{{i}}",
-    {% endfor %}
-};
-reconf['BLACKLIST_CUSTOMERS'] = {
-  re = string.format("(%s)", table.concat(blacklistedsenders, ") | (")),
-  group = group,
-  description = "not authorized senders",
-  score = 30000.0,
-}
-{% endif %}
-
-{% if cops_mailer_rspamd_lua_whitelisted %}
-local whitelistedsenders = {
-    {% for i in cops_mailer_rspamd_lua_whitelisted%}
-    "{{i}}",
-    {% endfor %}
-};
-reconf['WHITELIST_CUSTOMERS'] = {
-  re = string.format("(%s)", table.concat(whitelistedsenders, ") | (")),
-  group = group,
-  description = "authorized senders",
-  score = -30000.0,
-}
-{% endif %}
---
 reconf['testspam'] = {
   re="/(?:{{cops_mailer_rspamd_testspam_token}})/im{all_header}",
   group = group,
@@ -56,6 +58,16 @@ reconf['testspam'] = {
 -- redirect outgoing spam to a specific address for further manual processing by sysadmins
 local cusourdomains = {
   rspamd_regexp.create_cached('/[a-zA-Z0-9_.+-]+@{{cops_mailer_ourdomains.replace('\\', '')}}/im'),
+};
+local cusourdomainswhitelistdest = {
+{% for i in cops_mailer_sentry_dests%}
+  rspamd_regexp.create_cached('/{{i}}|/im'),
+{% endfor %}
+};
+local cusourdomainswhitelistexp = {
+{% for i in cops_mailer_sentry_dests%}
+  rspamd_regexp.create_cached('/{{i}}/im'),
+{% endfor %}
 };
 local is_on = rspamd_regexp.create_cached('/^(?:1|y(?:es)?)$/im');
 rspamd_config:register_symbol({
@@ -79,14 +91,13 @@ rspamd_config:register_symbol({
     aheaders = {
         ['X-Cops-OutSpam-isdomain'] = tostring(is_domain),
         ['X-Cops-OutSpam-Spam-pr'] = pr,
-        ['X-Outgoing-SpamH'] = is_bm_event:match(mh),
-        ['X-Outgoing-SpamC'] = is_bm_event:match(mc),
+        ['X-Cops-OutSpam-SpamH'] = is_bm_event:match(mh),
+        ['X-Cops-OutSpam-SpamC'] = is_bm_event:match(mc),
     };
     if is_spam and not is_domain and not is_a_bmevent then
       aheaders['X-Outgoing-Spam'] = '1';
     end
     task:set_milter_reply({add_headers = aheaders});
-
   end
 });
 --
